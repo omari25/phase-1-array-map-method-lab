79266d3a057bf1f2b6f310b3799d40658e43ddb1
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-13 11:23:27 +0300

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const array = tutorials.map((sentence) => {
+    const words = sentence.split(" ");
+    const wordTitle = words.map((word) =>
+      word.charAt(0).toUpperCase() + word.slice(1));
+    const result = wordTitle.join(" ");
+    return result;
+  })
+  return array;
 }
